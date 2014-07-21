b25e34def2a5554a53c11c218629dc830f6e98de
Marcos Placona <Marcos.Placona@hostelbookers.com>
2014-07-21 13:20:00 +0100

    Finalise page and template

--- main.py
+++ main.py
@@ -1,43 +1,41 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import webapp2
 import urllib2
 import json
+from google.appengine.api import memcache
+import datetime
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         url = "https://www.kimonolabs.com/api/8mjgyceu?apikey=5YBvcrTB43XXYfhpnXPneBlxTg2lpH4p"
+        client = memcache.Client()
+        date = datetime.datetime.now()
+        memcache_key = date.strftime('%Y%m%d')
         try:
-          result = urllib2.urlopen(url).read()
+          result = memcache.get(memcache_key)
+          if result is not None:
+              result = result
+          else:
+              result = urllib2.urlopen(url).read()
+              memcache.add(memcache_key, result)
+          
           retdict = json.loads(result)
           count = retdict['count']
           lastFetch = retdict['lastsuccess']
-          self.response.out.write("<html>")
-          self.response.out.write("<body>")
-          self.response.out.write("<h1>Dart pub stats</h1>")
-          self.response.out.write("<h2>There are currently ")
-          self.response.out.write(count)
-          self.response.out.write(" packages available</h2>")
-          self.response.out.write("<p>Data last fetched on ")
-          self.response.out.write(lastFetch)
-          self.response.out.write("</p>")
-          self.response.out.write("</body>")
-          self.response.out.write("</html>")
-          
+          template = """
+          <html>
+                <head>
+                    <title>Dart pub stats</title>
+                    <meta itemprop='image' content='https://www.dartlang.org/imgs/dart-logo-wordmark-1200w.png'>
+                    <meta itemprop='description' content='Statistical information about the Dart's Pub repositoty'>
+                </head>
+                <body>
+                        <h1>Dart pub stats</h1>
+                        <h2>There are currently %s packages available</h2>
+                        <p>Data last fetched on %s</p>
+                </body>
+          </html>
+          """
+          self.response.out.write(template %(count, lastFetch))
         except urllib2.URLError, e:
           print(e)
 
