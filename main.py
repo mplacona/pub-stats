e155b77d5028781154664708747d911744f335b4
Marcos Placona <Marcos.Placona@hostelbookers.com>
2014-07-23 09:54:02 +0100

    Markup cleanup and Analytics

--- main.py
+++ main.py
@@ -22,11 +22,24 @@ class MainHandler(webapp2.RequestHandler):
             count = retdict['count']
             lastFetch = retdict['lastsuccess']
             template = """
-            <html>
+            <!DOCTYPE html>
+            <html lang='en-us' itemscope itemtype='http://schema.org/Product'>
                 <head>
                     <title>Dart pub stats</title>
                     <meta itemprop='image' content='https://www.dartlang.org/imgs/dart-logo-wordmark-1200w.png'>
-                    <meta itemprop='description' content='Statistical information about the Dart's Pub repositoty'>
+                    <meta itemprop='name' content='Dart pub stats'>
+                    <meta name='description' content='Statistical information about the Dart&quot;s Pub repositoty'>
+                    <meta itemprop='description' content='Statistical information about the Dart&quot;s Pub repositoty'>
+                    <script>
+                      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+                      ga('create', 'UA-53102965-1', 'auto');
+                      ga('send', 'pageview');
+
+                    </script>
                 </head>
                 <body>
                     <h1>Dart pub stats</h1>
